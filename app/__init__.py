77bfd81cfc4836b563327c07ebec4eaf9e936e0a
my-website <isaiahmokua003@gmail.com>
2025-10-17 16:51:30 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask,render_templates
+from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
