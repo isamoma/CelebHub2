73e0d3e3b0d6d49b8062528b114fa4a31dbbd093
isamoma <isaiahmokua003@gmail.com>
2025-10-18 09:19:44 +0300

    files changed

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask,render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
