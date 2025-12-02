086b0a3af2443812e9f82c5c53db8c8b0fa52e18
isamoma <isaiahmokua003@gmail.com>
2025-12-02 18:10:42 +0300

    Added test.py

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-from sqlalchemy import create_engine
-engine = create_engine('sqlite:///your_database.db')
-with engine.connect() as conn:
-    result = conn.execute("PRAGMA table_info(onboarding_registration);")
-    for row in result:
-        print(row)
+app = Flask(__name__, instance_relative_config=True)
+@app.route('/mpesa/token')
+def generate_token():
+    import requests, os
+    from requests.auth import HTTPBasicAuth
+
+    key = os.getenv("MPESA_CONSUMER_KEY")
+    secret = os.getenv("MPESA_CONSUMER_SECRET")
+
+    url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+    response = requests.get(url, auth=HTTPBasicAuth(key, secret))
+
+    return response.json()
